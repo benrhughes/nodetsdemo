7ac2f27e2d3f51512d733c9f64a3c865f8c28ad3
Ben <ben@benrhughes.com>
2017-07-17 16:44:46 +1000

    basic REST routes

--- server.ts
+++ server.ts
@@ -2,10 +2,12 @@ import * as express from "express";
 
 export default class Server {
   private app: express.Application;
+  private data: {[index:string ]: string}
 
   constructor() {
     this.app = express();
     this.registerRoutes();
+    this.data = {};
   }
 
   public start(){
@@ -13,8 +15,23 @@ export default class Server {
   }
 
   private registerRoutes() {
-    this.app.get('/:name', (req, res) => {
-      res.json({'msg': 'Hello ' + req.params['name']});
+    this.app.get('/user', (req, res) =>{
+      return res.json(Object.keys(this.data));
+    });
+
+    this.app.get('/user/:name', (req, res) => {
+      var name = req.params['name'];
+      if(this.data[name])
+        return res.json(this.data[name]);
+      else
+        return res.sendStatus(404);
     })
+
+    this.app.post('/user/:name', (req, res) => {
+      var name = req.params['name'];
+      this.data[name] = 'Hello ' + name;
+      res.setHeader('location', '/user/'+ name);
+      return res.sendStatus(201);
+    });
   }
 }
